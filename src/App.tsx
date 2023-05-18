8387c885fc8173894ee829ba4281f86c561ea2b4
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2023-05-18 07:44:27 +0100

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+
 function App() {
-	return <>App</>;
+	return (
+		<>
+			<Grid
+				gridTemplateAreas={{
+					base: `"nav" "main"`,
+					lg: `"nav nav " "aside main" "footer footer" `,
+				}}>
+				<GridItem pl='2' bg='orange.300' area={'nav'}>
+					Nav
+				</GridItem>
+
+				<Show above='lg'>
+					<GridItem pl='2' bg='pink.300' area={'aside'}>
+						aside
+					</GridItem>
+				</Show>
+				<GridItem pl='2' bg='green.300' area={'main'}>
+					main
+				</GridItem>
+				<GridItem pl='2' bg='blue.300' area={'footer'}>
+					footer
+				</GridItem>
+			</Grid>
+		</>
+	);
 }
 
 export default App;
