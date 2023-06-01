da2d6f9bbd3d99c5b026aaab3388fb1cfcc5cd15
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2023-06-01 19:53:41 +0100

    Customize theme

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -7,6 +7,22 @@ const config: ThemeConfig = {
 };
 
 // 3. extend the theme
-const theme = extendTheme({ config });
+const theme = extendTheme({
+	config,
+	colors: {
+		gray: {
+			800: '#2d3748',
+			900: '#1a202c',
+			700: '#4a5568',
+			600: '#718096',
+			500: '#a0aec0',
+			400: '#cbd5e0',
+			300: '#e2e8f0',
+			200: '#edf2f7',
+			100: '#f7fafc',
+			50: '#f0f4f8',
+		},
+	},
+});
 
 export default theme;
